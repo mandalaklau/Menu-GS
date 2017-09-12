7e8b983a4c7b700a709864e1a19f11153c9d69d0
netoz <neto_z@hotmail.com>
2017-09-12 13:24:40 -0500

    Acomo de Archivos
    Se ponene los archivos en sus respectivas carpetas para visualizar el menú

--- menu_javascript.js
+++
@@ -1,9 +0,0 @@
-/* Alternar entre la adición y eliminación de la clase "responsive" a topnav cuando el usuario hace clic en el icono */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
